164750705454be5fcd4eec2bc079ce9b0dbaeadd
Antonio Santos <asantos071@gmail.com>
2022-02-05 23:40:56 -0300

    Se agrega @Getmappin con template.postForlocation y template.getForObject

--- src/main/java/com/controller/personasController.java
+++ src/main/java/com/controller/personasController.java
@@ -4,12 +4,8 @@ import com.model.*;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
