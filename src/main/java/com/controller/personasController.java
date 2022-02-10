2f8873923d4d7ac122823d7620b7b75f72d80966
Antonio Santos <asantos071@gmail.com>
2022-02-10 18:14:16 -0300

    Modificaciones para el control de exepciones.

--- src/main/java/com/controller/personasController.java
+++ src/main/java/com/controller/personasController.java
@@ -2,16 +2,21 @@ package com.controller;
 
 import com.model.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -23,14 +28,25 @@ public class personasController {
     private String urlBase = "http://localhost:8080";
 
     @GetMapping(value = "/personas/{nombre}/{email}/{edad}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Persona> altaPersona(@PathVariable("nombre") String nombre,
+    public ResponseEntity<List<Persona>> altaPersona(@PathVariable("nombre") String nombre,
             @PathVariable("email") String email,
             @PathVariable("edad") int edad) {
-
         Persona persona = new Persona(nombre, email, edad);
-        template.postForLocation(urlBase + "/contactos", persona);
-        Persona[] personas = template.getForObject(urlBase + "/contactos", Persona[].class);
-        return Arrays.asList(personas);
+        try {
+            template.postForLocation(urlBase + "/contactos", persona);
+            Persona[] personas = template.getForObject(urlBase + "/contactos", Persona[].class);
+            return new ResponseEntity<List<Persona>>(Arrays.asList(personas), HttpStatus.OK);
+
+        } catch (HttpStatusCodeException exception) {
+            // si hubo error en la llamada al microservicio, enviamos a nuestro cliente
+            // final
+            // una cabecera con el mensaje de error, una lista vacia de personas en el
+            // cuerpo
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("error", exception.getResponseBodyAsString());
+            return new ResponseEntity<List<Persona>>(new ArrayList<Persona>(), headers, exception.getStatusCode());
+
+        }
 
     }
 
