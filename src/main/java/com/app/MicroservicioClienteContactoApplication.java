72205b384f5dcd1846d91674a965008c2f1b01ec
Antonio Santos <asantos071@gmail.com>
2022-02-12 23:36:49 -0300

    Inicio de uso de Token JWT como cliente

--- src/main/java/com/app/MicroservicioClienteContactoApplication.java
+++ src/main/java/com/app/MicroservicioClienteContactoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -15,8 +16,11 @@ public class MicroservicioClienteContactoApplication {
 	}
 
 	@Bean
-	public RestTemplate template(){
-		return new RestTemplate();
+	public RestTemplate template() {
+		BasicAuthenticationInterceptor intercep = new BasicAuthenticationInterceptor("admin", "admin");
+        RestTemplate template = new RestTemplate();
+		template.getInterceptors().add(intercep);
+		return  template;
 	}
 
 }
